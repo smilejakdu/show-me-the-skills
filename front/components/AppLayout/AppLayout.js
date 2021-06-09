4261965c52f4e8f1812f553c1d011633ca5c2e7e
smilejakdu <ash982416@gmail.com>
2021-06-09 10:54:46 +0900

    front header bug modify

--- front/components/AppLayout/AppLayout.js
+++ front/components/AppLayout/AppLayout.js
@@ -8,25 +8,35 @@ import { NavLinkCustom, NavbarBrandCustom } from "./AppLayout.style";
 
 const AppLayout = ({children}) =>{
   return (
-    <Navbar bg="light" expand="lg" 
-    style={{borderBottom:"1px solid #ced4da",
-            boxShadow:"0 4px 6px -6px #222"
-    }}>
-      <Container>
-        <title>show me the skills</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <style dangerouslySetInnerHTML={{ __html: bootstrapStyle }} />
-        <NavbarBrandCustom href="/lesson"><a>Show Me the Skills</a></NavbarBrandCustom>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLinkCustom href="/lesson">Lesson</NavLinkCustom>
-            <NavLinkCustom href="/mentoring">Mentoring</NavLinkCustom>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <div>
+        <head>
+          <title>show me the skills</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <style dangerouslySetInnerHTML={{ __html: bootstrapStyle }} />
+        </head>
+      <Navbar
+        bg="light"
+        expand="lg"
+        style={{
+          borderBottom: "1px solid #ced4da",
+          boxShadow: "0 4px 6px -6px #222",
+        }}
+      >
+        <Container>
+          <Navbar.Brand href="/lesson">
+            <NavbarBrandCustom>Show Me the Skills</NavbarBrandCustom>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/lesson"><NavLinkCustom>Lesson</NavLinkCustom></Nav.Link>
+              <Nav.Link href="/mentoring"><NavLinkCustom>Mentoring</NavLinkCustom></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      </div>
   );
 };
 
--- front/components/AppLayout/AppLayout.style.js
+++ front/components/AppLayout/AppLayout.style.js
@@ -5,21 +5,27 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavbarBrandCustom = styled.a`
   color: none;
-  color: ${palette.green[1]};
-  font-size:20px;
   text-decoration: none;
+  color: ${palette.green[1]};
+  font-size: 20px;
+
+  &:hover {
+    color:none;
+    text-decoration: underline;
+    color: ${palette.green[1]};
+  }
 `;
 
 const NavLinkCustom = styled.a`
-  color:none;
-  text-decoration: none;
-  padding: 10px;
-  color: ${palette.base_color[2]};
-
-  &:hover {
-    text-decoration: underline;
+    text-decoration: none;
+    margin-right: 20px;
     color: ${palette.base_color[2]};
-  }
+
+    &:hover {
+      cursor: pointer;
+      text-decoration: underline;
+      color: ${palette.base_color[2]};
+    }
 `;
 
 

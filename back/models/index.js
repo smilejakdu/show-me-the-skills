3899b50494a7dfff5c8d36fb929e92ac7afa9b65
smilejakdu <ash982416@gmail.com>
2021-06-10 11:09:45 +0900

    back models index

--- back/models/index.js
+++ back/models/index.js
@@ -1,32 +1,24 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const user = require('./user');
+const product = require('./product')
+
+const env = process.env.NODE_ENV || 'development'; // 개발하게 될땐 development 가 동작 배포할땐 production
+const config = require('../config/config')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  config.database,
+  config.username ,
+  config.password ,
+  config
+)
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+db.User = user;
+db.Product = product;
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
+
+Object.keys(db).forEach(modelName =>{
+  if(db[modelName].associate){
     db[modelName].associate(db);
   }
 });
